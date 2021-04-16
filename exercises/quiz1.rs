822b2f1b8c8db526e7f3daf828d9e0faf5da0741
Triet <triet.nguyen@centrality.ai>
2021-04-16 12:54:35 +1200

    semantics

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,20 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn ..... {
+
+fn calculate_apple_price(apples: i8) -> i8 {
+    match apples {
+        apples => {
+            if apples > 40 {
+                apples * 1
+            } else {
+                apples * 2
+            }
+        }
+    }
+}
 
 // Don't modify this function!
 #[test]
